dd6511b599ee06afcf74e032c6520a49fa893d80
AHMAD FAIZIN <044223886@ecampus.ut.ac.id>
2024-12-03 02:59:30 +0800

    faizin

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="/" class="brand-link">
-        <span class="brand-text font-weight-light">Admin</span>
+        <span class="brand-text font-weight-light">Lansia Sehat</span>
     </a>
     <div class="sidebar">
         <nav class="mt-2">
